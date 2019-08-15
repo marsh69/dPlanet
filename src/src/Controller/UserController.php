015796b0160dfce4e3025334fdaa4440c5f5781b
Ishank Sharma <45262592+ishank-dev@users.noreply.github.com>
2019-08-15 18:58:55 +0530

    Update UserController.php

--- src/src/Controller/UserController.php
+++ src/src/Controller/UserController.php
@@ -1,6 +1,5 @@
 <?php
 namespace App\Controller;
-
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
