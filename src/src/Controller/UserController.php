36ccb0d4d45f67fea1d2b09a678a453d7c160103
Ishank <ishankdev@gmail.com>
2019-08-15 17:50:52 +0530

    add profile page

---
+++ src/src/Controller/UserController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UserController extends AbstractController
+{
+    /**
+     *
+     * @param Request $request
+     * @return Response
+     */
+    public function index(Request $request)
+    {   $name = $request->get('name');
+        return $this->render('user/index.html.twig',[
+            'name' => $name
+        ]);
+
+    }
+}
