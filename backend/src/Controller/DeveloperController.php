9e291a165f52d3e6ad1b1908dc61135af983e640
survivorbat <djbatcat@gmail.com>
2019-08-17 22:02:12 +0200

    develop: Remove auth from register route. add a few more settings

--- backend/src/Controller/DeveloperController.php
+++ backend/src/Controller/DeveloperController.php
@@ -9,22 +9,46 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Intl\Exception\NotImplementedException;
 
-class UserController extends AbstractFOSRestController
+class DeveloperController extends AbstractFOSRestController
 {
     /**
+     * TODO: Finish this
+     *
      * @SWG\Get(
      *     summary="Get all users",
      *     produces={"application/json"},
      *     @SWG\Response(
      *         response=200,
      *         description="Success",
-     *         @Model(type=App\Entity\Trend::class, groups={"user"})
+     *         @Model(type=App\Entity\Developer::class, groups={"user"})
      *    )
      * )
      * @param Request $request
      * @return Response
      */
     public function index(Request $request): Response
+    {
+        throw new NotImplementedException('This controller is not yet done');
+    }
+
+    /**
+     * TODO: Finish this
+     *
+     * @SWG\Post(
+     *     security={},
+     *     summary="Register a new user",
+     *     produces={"application/json"},
+     *     @SWG\Response(
+     *         response=200,
+     *         description="Success",
+     *         @Model(type=App\Entity\Developer::class, groups={"user"})
+     *    )
+     * )
+     *
+     * @param Request $request
+     * @return Response
+     */
+    public function create(Request $request): Response
     {
         throw new NotImplementedException('This controller is not yet done');
     }
